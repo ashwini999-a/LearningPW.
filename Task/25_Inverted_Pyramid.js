52173a87cb76f63c55fe522c7668bfb1c06c7b56
Ashwini Karale <ashwinirjadhav1985@gmail.com>
2026-03-26 18:57:14 +0530

    Add Objects chapter, Multi-Dimension Array chapter, and Task exercises

---
+++ Task/25_Inverted_Pyramid.js
@@ -0,0 +1,30 @@
+//*********
+
+//*******
+
+//*****
+
+// ***
+
+// *
+
+
+let n = 5;
+for (let i = n; i >= 1; i--) {
+    let row = " ";
+    for (j = 1; j <= 2 * i - 1; j++) {
+        row = row + "* ";
+
+    }
+    console.log(row.trim());
+}
+
+
+/*let n = 5;
+for (let i = n; i >= 1; i----) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += "*";
+    }
+    console.log(row);
+}*/
--- chapter_12_Multi_Dimension_Array/123_MD_Pattern_RIGHT.js
+++ chapter_12_Multi_Dimension_Array/123_MD_Pattern_RIGHT.js
@@ -4,12 +4,33 @@
 // * *
 // * * *
 
-let n = 3;
+/*let n = 3;
 for (let i = 1; i <= n; i++) {
     let row = " ";
     for (let j = 1; j <= i; j++) {
         //row = row + "* ";
         row += "* ";
     }
+    console.log(row.trim());
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let n = 3;
+for (i = 1; i <= n; i++) {
+    let row = " ";
+    for (j = 0; j < i; j++)
+        row = row + "*"
     console.log(row.trim());
 }
